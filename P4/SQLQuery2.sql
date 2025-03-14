c8b5865303eb591b0f1925a30f284528fb77ff25
Lazarosa04 <lazarosa@ua.pt>
2025-03-14 23:16:53 +0000

    exc

---
+++ P4/SQLQuery2.sql
@@ -0,0 +1,52 @@
+﻿CREATE database Farmacia
+use Farmacia
+GO
+
+--medico
+create table Medico(
+	SNS				char(9)		PRIMARY KEY,
+	Nome			varchar(30)	not null,
+	Especialidade	varchar(30),
+)
+
+--paciente
+create table Paciente(
+	N_utente	int			PRIMARY KEY,	
+	Nome		varchar(30),
+	Data_nascimento date,
+)
+
+--farmaceutica
+create table Farmaceutica(
+	N_R_Nacional	int	PRIMARY KEY,
+	Nome			varchar(30),
+	Endereço		varchar(30),
+	Telefone		char(9),
+)
+
+--farmaco
+create table Farmaco(
+	Formula		varchar(30)		PRIMARY KEY,
+	Nome_comercial	varchar(30),
+	N_R_Nacional	int	references Farmaceutica(N_R_Nacional)
+)
+
+--prescrição
+create table Prescrição(
+	N_prescricao	int		PRIMARY KEY,
+	Data_prescricao	date,
+	SNS				char(9)	references Medico(SNS),
+	N_utente		int		references Paciente(N_utente),
+	Formula			varchar(30)	references Farmaco(Formula),
+)
+
+
+--farmácia
+create table Farmacia(
+	NIF			char(9)		PRIMARY KEY,
+	Nome			varchar(30),
+	Endereço		varchar(30),
+	Telefone		char(9),
+	N_prescricao	int references Prescrição(N_prescricao),
+	DataProc		date
+)
---
+++ P4/SQLQuery3.sql
@@ -0,0 +1,41 @@
+﻿CREATE database Stock
+use Stock
+GO
+
+--Tipo Fornecedor
+create table Tipo_Fornecedor(
+	Codigo			char(9)		PRIMARY KEY,
+	Designacao		varchar(30)	not null,
+)
+
+--Fornecedor
+create table Fornecedor(
+	NIF			char(9)		PRIMARY KEY,
+	Nome		varchar(30),
+	Endereço	varchar(30),
+	Fax			char(9)	not null,
+	Codigo		char(9)	references Tipo_Fornecedor(Codigo),
+)
+
+--Produto
+create table Produto(
+	Codigo_produto	char(9)	PRIMARY KEY,
+	Nome			varchar(30),
+	Preço			int not null,
+	Iva				int not null,
+	Unidades		int,
+)
+
+--Encomenda
+create table Encomenda(
+	N_encomenda		int		PRIMARY KEY,
+	Data_encomenda	date,
+	Codigo_produto	char(9)	references Produto(Codigo_produto),
+	NIF_Fornecedor	char(9)	references Fornecedor(NIF)
+)
+
+--Armazem
+create table Armazem(
+	Nome			varchar(30)	PRIMARY KEY,
+	Codigo_produto	char(9)	references Produto(Codigo_produto),
+)
